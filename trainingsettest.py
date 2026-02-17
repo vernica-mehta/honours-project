5c4da65ba0c9ce62ff543b6c473e232cae7fd83c
Vernica Mehta <u7481637@anu.edu.au>
2026-02-18 10:40:26 +1100

    moving some more things around

--- trainingsettest.py
+++ trainingsettest.py
@@ -1,8 +1,8 @@
 import os
 import numpy as np
 from astropy.io import fits
-from pyght.src.AnniesLasso.thecannon.vectorizer.polynomial import PolynomialVectorizer
-from pyght.src.AnniesLasso.thecannon.model import CannonModel
+from code.src.AnniesLasso.thecannon.vectorizer.polynomial import PolynomialVectorizer
+from code.src.AnniesLasso.thecannon.model import CannonModel
 
 class UniformCannonTrainer:
 
